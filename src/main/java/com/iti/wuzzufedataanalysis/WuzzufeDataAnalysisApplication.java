33c94fb9a591ba7a30fc837b14e3bcec8c9aec3f
DESKTOP-L8DD1MM\light <ahmed.m.3bd.elghani@gmail.com>
2021-06-27 11:31:07 +0200

    Initialize Project

---
+++ src/main/java/com/iti/wuzzufedataanalysis/WuzzufeDataAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.iti.wuzzufedataanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuzzufeDataAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WuzzufeDataAnalysisApplication.class, args);
+    }
+
+}
